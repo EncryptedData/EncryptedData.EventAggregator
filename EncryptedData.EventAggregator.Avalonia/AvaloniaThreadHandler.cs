f09384748df5d143ab39c2166ef627242cd33d1f
russell.johnson <encrypteddata0@gmail.com>
2022-05-06 07:58:55 -0700

    [Avalonia] Added Avalonia support

---
+++ EncryptedData.EventAggregator.Avalonia/AvaloniaThreadHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Threading;
+
+namespace EncryptedData.EventAggregator.Avalonia
+{
+    public class AvaloniaThreadHandler : IEventThreadHandler
+    {
+        public Task Invoke(Task task)
+        {
+            return Dispatcher.UIThread.InvokeAsync(() => task);
+        }
+    }
+}
